29fa21627bd70850a786e4081a8659af5ee8ee4d
Bruna Delmouro <Bruna.Delmouro-ext@ab-Inbev.com>
2022-09-23 23:37:15 -0300

    11. Concluindo e testando o microservice de clientes

--- msclientes/src/main/java/br/com/brunadelmouro/msclientes/service/ClienteService.java
+++ msclientes/src/main/java/br/com/brunadelmouro/msclientes/service/ClienteService.java
@@ -3,6 +3,7 @@ package br.com.brunadelmouro.msclientes.service;
 import br.com.brunadelmouro.msclientes.infra.repository.ClienteRepository;
 import br.com.brunadelmouro.msclientes.model.Cliente;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,8 +20,10 @@ public class ClienteService {
         return clienteRepository.save(cliente);
     }
 
-    public Optional<Cliente> findByCpf(String cpf) {
-        return clienteRepository.findByCpf(cpf);
+    public Cliente findByCpf(String cpf) {
+        Optional<Cliente> cliente = clienteRepository.findByCpf(cpf);
+
+        return cliente.orElseThrow(() ->  new ObjectNotFoundException(1, "Not Found"));
     }
 
 }
