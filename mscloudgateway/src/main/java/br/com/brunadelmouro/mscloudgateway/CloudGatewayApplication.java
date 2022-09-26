cd860d61890fa47e28c9b46fe7b46bf9b9bf6d32
Bruna Delmouro <brunadelmouro@gmail.com>
2022-09-25 21:45:16 -0300

    12 - Configuração e Implementação do Gateway

---
+++ mscloudgateway/src/main/java/br/com/brunadelmouro/mscloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,34 @@
+package br.com.brunadelmouro.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+/*
+ * Doc: https://spring.io/projects/spring-cloud-gateway
+ * */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class CloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+
+    /**
+     * register Gateway on Eureka Server, to identify the registered
+     * microservices(instances of msclientes) and also to do the load balancer.
+     */
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder routeLocator) {
+        return routeLocator
+                .routes()
+                .route(route -> route.path("/clientes/**").uri("lb://msclientes"))
+                .build();
+    }
+}
