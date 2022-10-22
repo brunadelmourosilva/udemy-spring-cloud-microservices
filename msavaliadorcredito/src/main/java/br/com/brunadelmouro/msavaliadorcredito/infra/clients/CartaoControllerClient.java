645f7ea4a80fa78abfc6b0eb22797e3598a49052
Bruna Delmouro <Bruna.Delmouro-ext@ab-Inbev.com>
2022-10-22 16:29:18 -0300

    25 - Buscando os dados de cartoes pelo microservice de cartoes

---
+++ msavaliadorcredito/src/main/java/br/com/brunadelmouro/msavaliadorcredito/infra/clients/CartaoControllerClient.java
@@ -0,0 +1,19 @@
+package br.com.brunadelmouro.msavaliadorcredito.infra.clients;
+
+import br.com.brunadelmouro.msavaliadorcredito.model.CartaoCliente;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+// url faz referência ao nome do microservice presente no load balancer do gateway
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartaoControllerClient {
+
+    // adiciona-se a mesma assinatura do método à ser consumido
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<CartaoCliente>> getCardsByCustomerCpf(@RequestParam("cpf") String cpf);
+}
--- msavaliadorcredito/src/main/java/br/com/brunadelmouro/msavaliadorcredito/service/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/br/com/brunadelmouro/msavaliadorcredito/service/AvaliadorCreditoService.java
@@ -1,5 +1,6 @@
 package br.com.brunadelmouro.msavaliadorcredito.service;
 
+import br.com.brunadelmouro.msavaliadorcredito.infra.clients.CartaoControllerClient;
 import br.com.brunadelmouro.msavaliadorcredito.infra.clients.ClienteControllerClient;
 import br.com.brunadelmouro.msavaliadorcredito.model.SituacaoCliente;
 import lombok.RequiredArgsConstructor;
@@ -9,17 +10,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
-    private final ClienteControllerClient client;
+    private final ClienteControllerClient clientClient;
+    private final CartaoControllerClient cardClient;
 
     public SituacaoCliente getCustomerSituation(String cpf) {
         //obter dados do cliente - microservice MSCLIENTES
         //obter cartões do cliente - microservice MSCARTOES
 
-        final var customerResponse = client.getCustomerByCpf(cpf);
+        final var customerResponse = clientClient.getCustomerByCpf(cpf);
+        final var cardResponse = cardClient.getCardsByCustomerCpf(cpf);
 
         return SituacaoCliente
                 .builder()
                 .cliente(customerResponse.getBody())
+                .cartoes(cardResponse.getBody())
                 .build();
     }
 }
